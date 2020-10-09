c46f7c98003c738d9b8ac110da4ff3923843af09
SDGreen <sad_green@hotmail.co.uk>
2020-10-09 12:20:27 +0000

    enviroment varibles setup in env.py file

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask
 from flask_pymongo import PyMongo
 
+if os.path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = "spooky_spool"
