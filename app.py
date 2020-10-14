56d84196f901ad51411b659391d570132aa498a7
SDGreen <sad_green@hotmail.co.uk>
2020-10-14 21:41:04 +0000

    bug found, non horror movies still in the database

--- app.py
+++ app.py
@@ -16,15 +16,22 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
+    all_movies = mongo.db.movies
 
-    return render_template("base.html")
+    movies = all_movies.find()
+
+    horror_movies = all_movies.find({"genre": {"$elemMatch": {"$eq": ObjectId("5f806ebc0727bbf597c35ba4")}}})
+
+    thriller_movies = all_movies.find({"genre": {"$elemMatch": {"$eq": ObjectId("5f806ebc0727bbf597c35ba5")}}})
+
+    return "Normal movies: " + str(movies.count()) + " Horror Movies: " + str(horror_movies.count()) + " Thriller Movies: " + str(thriller_movies.count())
 
 @app.route("/browse")
 def browse_movies():
 
     movie_list = mongo.db.movies
 
-    movies = movie_list.find().sort("year", -1).limit(30)
+    movies = movie_list.find().sort("year", -1).limit(60)
 
     return render_template("browse.html", movies=movies)
 
