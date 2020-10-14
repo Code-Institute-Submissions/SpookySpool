203e4c3c411aad84b2b1f8f36d2828d7ef08c5a7
SDGreen <sad_green@hotmail.co.uk>
2020-10-14 12:22:54 +0000

    backup of movie data created before genres are updated

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from bson.json_util import dumps
 
 if os.path.exists("env.py"):
     import env
@@ -17,18 +18,22 @@ mongo = PyMongo(app)
 @app.route("/")
 def index():
     movie_db = mongo.db.movies
+    genres = mongo.db.genres.find()
     movies = movie_db.find()
-    zombie_movies = []
-    
-    for movie in movies:
-        if ("zombie" in str(movie["description"])) or ("Zombie" in str(movie["description"])):
-            zombie_movies.append(movie["title"])
-            movie_db.update({"_id": ObjectId(movie["_id"])},{"$addToSet":{"genre": "Zombie"}})
-    
-    length = len(zombie_movies)
+    list_movies = list(movies)
+    json_movies = dumps(list_movies, indent = 2)
+    with open('data.json', 'w') as file:
+        file.write(json_movies)
 
-    return str(length) + str(zombie_movies)
+    """for genre in genres:
 
+        movie_db.update_many(
+            {"genre": genre["genre_name"]},
+            {"$set": {"genre.$": ObjectId(genre["_id"])}}
+        )"""
+
+
+    return "movie back-up created before genre update"
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
