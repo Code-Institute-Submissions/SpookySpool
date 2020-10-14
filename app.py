4497665bde8fdf08d2f7fe3a120088b596abbea6
SDGreen <sad_green@hotmail.co.uk>
2020-10-14 12:28:14 +0000

    genres updated in movie genre array to id's from genre collection

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import os
-import json
 from flask import Flask
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-from bson.json_util import dumps
 
 if os.path.exists("env.py"):
     import env
@@ -15,25 +13,20 @@ app.config["MONGO_URI"] = os.getenv("MONGODB_URI")
 
 mongo = PyMongo(app)
 
+
 @app.route("/")
 def index():
     movie_db = mongo.db.movies
     genres = mongo.db.genres.find()
-    movies = movie_db.find()
-    list_movies = list(movies)
-    json_movies = dumps(list_movies, indent = 2)
-    with open('data.json', 'w') as file:
-        file.write(json_movies)
 
-    """for genre in genres:
-
+    for genre in genres:
         movie_db.update_many(
             {"genre": genre["genre_name"]},
             {"$set": {"genre.$": ObjectId(genre["_id"])}}
-        )"""
+        )
 
+    return "Genres updated in movie genre arrays to genre ids from genre collection"
 
-    return "movie back-up created before genre update"
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
